750ac093feb9f4e5d92875b4b775f93bd80e1755
Harivansh Mehta <harivanshmehta4@gmail.com>
2025-07-13 14:25:32 +0530

    Add the logic for playing a round

--- script.js
+++ script.js
@@ -23,11 +23,27 @@ function playRound(humanChoice, computerChoice) {
     }
     else {
         if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "rock")) {
-            console.log("You win! The computer loses!");
+            console.log("You win this round! The computer loses!");
+            humanScore++;
         }
         else {
-            console.log("Hard luck! Y")
+            console.log("Hard luck! You lose this round!")
+            computerScore++;
         }
     }
+}
 
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    if (humanScore > computerScore) {
+        alert("Congratulations! You have won the game!");
+    }
+    else if (humanScore == computerScore) {
+        alert("The game has been tied.");
+    }
+    else {
+        alert("Hard luck! The computer has won the game!");
+    }
 }
