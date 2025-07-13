8403df55bf70a8868347282ea719be3126855573
Harivansh Mehta <harivanshmehta4@gmail.com>
2025-07-13 17:24:30 +0530

    Change JavaScript code; functionally complete.

--- script.js
+++ script.js
@@ -9,26 +9,40 @@ function getComputerChoice() {
     return "scissors";
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice (rock/paper/scissors):");
-    return choice;
-}
 
 let humanScore = 0, computerScore = 0;
+let computerScoreDiv = document.querySelector(".computer-score");
+let humanScoreDiv = document.querySelector(".human-score");
+let resultDiv = document.querySelector(".result");
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     if (humanChoice == computerChoice) {
-        console.log("Round tied!");
+        resultDiv.textContent = "Round tied!";
     }
     else {
         if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "rock")) {
-            console.log("You win this round! The computer loses!");
+            resultDiv.textContent = "You win this round! The computer loses!";
             humanScore++;
+            humanScoreDiv.textContent = `Human Score: ${humanScore}`;
         }
         else {
-            console.log("Hard luck! You lose this round!")
+            resultDiv.textContent = "Hard luck! You lose this round!";
             computerScore++;
+            computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
         }
     }
+    if (humanScore == 5) {
+        alert("Congratulations! You have won the game!");
+    }
+    else if (computerScore == 5) {
+        alert("Hard luck! You have lost the game!")
+    }
 }
+
+document.querySelector("#rock").addEventListener("click", () => playRound("rock", getComputerChoice()));
+document.querySelector("#paper").addEventListener("click", () => playRound("paper", getComputerChoice()));
+document.querySelector("#scissors").addEventListener("click", () => playRound("scissors", getComputerChoice()));
